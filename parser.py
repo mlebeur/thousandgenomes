382f80afd545a6e1a2696511fb7840919a729a1b
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-02-26 17:06:47 +0100

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,19 @@
+import os, pandas, csv, re
+import numpy as np
+from biothings.utils.dataload import dict_convert, dict_sweep
+from biothings import config
+logging = config.logger
+def load_thousandgenomes(data_folder):
+    infile = os.path.abspath("/opt/biothings/GRCh37/thousand_genomes/phase3/ThousandGenomes.tsv")
+    assert os.path.exists(infile)
+    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
+    results = {}
+    for rec in dat:
+        _id = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]       
+        process_key = lambda k: k.replace(" ","_").lower()
+        rec = dict_convert(rec,keyfn=process_key)
+        rec = dict_sweep(rec,vals=[np.nan])
+        results.setdefault(_id,[]).append(rec)
+    for _id,docs in results.items():
+        doc = {"_id": _id, "thousandgenomes" : docs}
+        yield doc
