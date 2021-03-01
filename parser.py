be91c13d90e271526523ea8eadf327f8ad2af063
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-03-01 15:02:39 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import os, pandas, csv, re
 import numpy as np
+import hashlib
 from biothings.utils.dataload import dict_convert, dict_sweep
 from biothings import config
 logging = config.logger
@@ -9,7 +10,8 @@ def load_thousandgenomes(data_folder):
     dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
     results = {}
     for rec in dat:
-        _id = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]       
+        var = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]       
+        _id = hashlib.sha224(var.encode('ascii')).hexdigest()       
         process_key = lambda k: k.replace(" ","_").lower()
         rec = dict_convert(rec,keyfn=process_key)
         rec = dict_sweep(rec,vals=[np.nan])
